8e0a7d8936e0f78acc14914c18470d7b86865fba
haraldkl <harald@klimachs.de>
2026-02-24 08:01:26 +0000

    Deploying to gh-pages from @ apes-suite/musubi@2dce7101b0f6f00f968a200050d5f54ad32fab53 🚀

--- src/mus_auxFieldVar_module.f90
+++ src/mus_auxFieldVar_module.f90
@@ -514,8 +514,8 @@ contains
     ! vectorization
     nChunks = ceiling(real(nSolve, kind=rk) / real(vlen, kind=rk))
 
-    !$omp parallel do                                                                            &
-    !$omp   & private( rho, pdf, elemPos, elemOff, nChunkElems, iChunks, low_bound, iElem, vel ) &
+    !$omp parallel do                                                                          &
+    !$omp   & private( rho, pdf, elemPos, elemOff, nChunkElems, iChunks, low_bound, iElem, vel )
     do iChunks = 1, nChunks
       ! calculate the end  number of iElem loop
       nChunkElems = min(vlen, nSolve - ((iChunks - 1) * vlen))
@@ -606,8 +606,8 @@ contains
     ! vectorization
     nChunks = ceiling(real(nSolve, kind=rk) / real(vlen, kind=rk))
 
-    !$omp parallel do                                                                            &
-    !$omp   & private( rho, pdf, elemPos, elemOff, nChunkElems, iChunks, low_bound, iElem, vel ) &
+    !$omp parallel do                                                                          &
+    !$omp   & private( rho, pdf, elemPos, elemOff, nChunkElems, iChunks, low_bound, iElem, vel )
     do iChunks = 1, nChunks
       ! calculate the end  number of iElem loop
       nChunkElems = min(vlen, nSolve - ((iChunks - 1) * vlen))
@@ -699,8 +699,8 @@ contains
     ! vectorization
     nChunks = ceiling(real(nSolve, kind=rk) / real(vlen, kind=rk))
 
-    !$omp parallel do                                                                            &
-    !$omp   & private( rho, pdf, elemPos, elemOff, nChunkElems, iChunks, low_bound, iElem, vel ) &
+    !$omp parallel do                                                                          &
+    !$omp   & private( rho, pdf, elemPos, elemOff, nChunkElems, iChunks, low_bound, iElem, vel )
     do iChunks = 1, nChunks
       ! calculate the end  number of iElem loop
       nChunkElems = min(vlen, nSolve - ((iChunks - 1) * vlen))
@@ -805,8 +805,8 @@ contains
     ! vectorization
     nChunks = ceiling(real(nSolve, kind=rk) / real(vlen, kind=rk))
 
-    !$omp parallel do                                                                            &
-    !$omp   & private( rho, pdf, elemPos, elemOff, nChunkElems, iChunks, low_bound, iElem, vel ) &
+    !$omp parallel do                                                                          &
+    !$omp   & private( rho, pdf, elemPos, elemOff, nChunkElems, iChunks, low_bound, iElem, vel )
     do iChunks = 1, nChunks
       ! calculate the end  number of iElem loop
       nChunkElems = min(vlen, nSolve - ((iChunks - 1) * vlen))
